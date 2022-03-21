9046ba081e40018e310f7cdc073ac0a3124cf0f0
agres <ale.agresta46@gmail.com>
2022-03-21 15:35:01 +0100

    fix

--- resources/js/components/PostCard.vue
+++ resources/js/components/PostCard.vue
@@ -5,20 +5,32 @@
       <div class="card-body">
         <h5 class="card-title">{{ post.title }}</h5>
         <p class="card-text" v-html="post.content"></p>
+        
+        <em>
+          Autore: {{ post.user.name }}; Data:
+          {{ formatDate(post.created_at) }}
+        </em>
 
-        <em>Autore: {{ post.user.name }}; Data: {{ post.created_at }}</em
-        ><br />
-        <strong v-if="post.category">{{ post.category.code }}</strong>
+      <strong v-if="post.category">{{ post.category.code }}</strong>
       </div>
+      <div v-if="post.tags">
+          <span
+            v-for="tag in post.tags"
+            :key="tag.id"
+            class="badge bg-secondary me-2"
+            >{{ tag.name }}</span>
+        </div>
 
-      <div class="card-footer">
-        <a href="#">Dettagli</a>
+      <div class="card-footer text-end">
+        <router-link :to="{ name:'posts.show', params: { post: post.slug } }">Dettagli<i class="fas fa-chevron-right ms-2"></i></router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import dayjs from "dayjs";
+
 export default {
   props: {
     post: Object,
@@ -29,6 +41,10 @@ export default {
         post.coverImg ??
         "https://blumagnolia.ch/wp-content/uploads/2021/05/placeholder-126.png"
       );
+    },
+    formatDate(date) {
+      console.log(date);
+      return dayjs(date).format("DD/MM/YYYY HH:mm");
     },
   },
 };
